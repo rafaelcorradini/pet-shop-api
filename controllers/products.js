a20bad65575cfefc9e3f937af53447804e79600a
Rafael <rafacunhadini@gmail.com>
2018-07-03 21:21:01 -0300

    products endpoints

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,29 @@
-// import schemas from '../schemas';
-
+import schemas from '../schemas';
 const db = require('../db');
 
-// exports.create = schemas.validating('product', create);
 
-// function create(product, cb) {  
-//   products.insert(product, product.email, cb);
-// }
+exports.create = schemas.validating('product', req.body, create);
 
-exports.create = (req, res) => {
-    db.insert('products', req.body).then( ({data, status}) => {
+create = (req, res) => {
+    if (!validate)
+        return res.status(400).json({message: "Validation error.", error: err});
+
+    return db.insert('products', req.body).then( ({data, status}) => {
         if (status != 201)
-            return res.status(400).json({message: "Validation error.", error: err});
+            return res.status(400).json({message: "DB error.", error: err});
 
 		return res.status(201).json(data);
     });
 }
 
 exports.getAll = (req, res) => {
-    db.get('products').then( ({data, status}) => {
+    const query = {
+        include_docs: true
+    }
+    return db.get('products', '_all_docs', query).then( ({data, status}) => {
         if (status != 200)
-            return res.status(400).json({message: "Validation error.", error: err});
+            return res.status(400).json({message: "DB error.", error: err});
 
-		return res.status(200).json(data);
+		return res.status(200).json(data.rows);
     });
 }
--- schemas/product.js
+++ schemas/product.js
@@ -1,9 +1,6 @@
 import Joi from 'joi';
 
 module.exports = Joi.object().keys({  
-  email: Joi.string().email().required(),
-  username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/),
-  access_token: [Joi.string(), Joi.number()],
-  birthyear: Joi.number().integer().min(1900).max((new Date()).getFullYear()),
+  name: Joi.string().required(),
+  description: Joi.string().required()
 });
